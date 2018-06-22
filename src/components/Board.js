b1d845691559c1d3a3bd162664cdf3a98f4e6b29
Arihant Ranka <arihantranka1701@gmail.com>
2018-06-22 14:03:27 +0530

    Setting difficulty level

--- src/components/Board.js
+++ src/components/Board.js
@@ -22,17 +22,60 @@ class Board extends React.Component {
         color: null,
         isBotTurn: isBotTurn,
         filledCount: 0,
-        mode: props.mode
+        mode: props.mode,
+        difficultyLevel : props.difficultyLevel,
+        piece: props.piece
       };
       this.handleBotTurn = this.handleBotTurn.bind(this);
+      this.getPossibleWinScenario = this.getPossibleWinScenario.bind(this);
+    }
+    possibleWinScenario = null;
+    getPossibleWinScenario() {
+      let possibleWinScenario = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+      if(this.state.difficultyLevel === "med") {
+        possibleWinScenario = [
+          [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],
+          [2,1,0],[5,4,3],[8,7,6],[6,3,0],[7,4,1],[8,5,2],[8,4,0],[6,4,2]
+        ];
+      } else if(this.state.difficultyLevel === "diff") {
+        possibleWinScenario = [
+          [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],
+          [2,1,0],[5,4,3],[8,7,6],[6,3,0],[7,4,1],[8,5,2],[8,4,0],[6,4,2],
+          [0,2,1],[3,5,4],[6,8,7],[6,0,3]
+        ];
+      }
+      return possibleWinScenario;
     }
     handleBotTurn() {
+      let move;
+      let botPiece = (this.state.piece === 'X' ) ? '0' : 'X';
+      for (let i = 0; i < this.possibleWinScenario.length; i++) {
+        const [a, b, c] = this.possibleWinScenario[i];
+        if((this.state.squares[a] === botPiece) && (this.state.squares[a] === this.state.squares[b]) && !this.state.squares[c]) {
+          move = c;
+          break;
+        }
+        if((this.state.squares[a] === this.state.piece) && (this.state.squares[a] === this.state.squares[b]) && !this.state.squares[c]) {
+          move = c;
+          break;
+        }
+      }
+      if(move>-1 && !this.state.squares[move]) { 
+        this.handleClick(move);
+      } else {
+        this.makeOrdinaryMove();
+      }
+    }
+    makeOrdinaryMove() {
       do{
         var randomNo = Math.round(Math.random()*8);
       } while(this.state.squares[randomNo])
       this.handleClick(randomNo);
     }
     componentDidMount() {
+      if(this.state.mode === "one") {
+        this.possibleWinScenario = this.getPossibleWinScenario();
+      }
       if(this.state.isBotTurn) {
         var that = this;
         setTimeout( function() {
@@ -54,7 +97,7 @@ class Board extends React.Component {
       if (calculateWinner(squares) || squares[i]) {
         return;
       }
-      squares[i] = this.state.xIsNext ? 'X' : 'O'; //taking turns
+      squares[i] = this.state.xIsNext ? 'X' : '0'; //taking turns
       let isBotTurn;
       if(this.state.mode === "two" ) {
         isBotTurn = false;
@@ -99,7 +142,7 @@ class Board extends React.Component {
         } else if(this.state.filledCount === 9) {
           status = "Match Drawn";	
         } else {
-          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); //display who's next 
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : '0'); //display who's next 
         }
   
       return (
--- src/components/Game.js
+++ src/components/Game.js
@@ -3,66 +3,86 @@ import '../ticTacToe.css';
 import Board from './Board';
 
 class Game extends Component {
-  	constructor(props) {
-		super(props);
-		this.state = {
-            mode: null, piece: null, turn: null, gotGameInfo: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: null, piece: null, turn: null, difficultyLevel: null, gotGameInfo: false
         };
-		this.handleModeChange = this.handleModeChange.bind(this);
-		this.handlePieceChange = this.handlePieceChange.bind(this);
-		this.handleTurnChange = this.handleTurnChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-	handleModeChange(event) {
-		this.setState({mode: event.target.value});
-	}
-	handlePieceChange(event) {
-		this.setState({piece: event.target.value});
-	}
-	handleTurnChange(event) {
-		this.setState({turn: event.target.value});
-	}
-    handleSubmit(event) {
-		if(this.state.piece && this.state.turn) {
-			this.setState({gotGameInfo: true});
-		}
-		event.preventDefault();	
+        this.handleModeChange = this.handleModeChange.bind(this);
+        this.handlePieceChange = this.handlePieceChange.bind(this);
+        this.handleTurnChange = this.handleTurnChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.setDifficultyLevel = this.setDifficultyLevel.bind(this);
     }
-	reset() {
-		this.setState({mode: null, piece: null, turn: null, gotGameInfo: false});
-	}
+    handleModeChange(event) {
+        this.setState({ mode: event.target.value });
+    }
+    handlePieceChange(event) {
+        this.setState({ piece: event.target.value });
+    }
+    handleTurnChange(event) {
+        this.setState({ turn: event.target.value });
+    }
+    handleSubmit(event) {
+        if (this.state.piece && this.state.turn) {
+            if (this.state.mode === "two" || (this.state.mode === "one" && this.state.difficultyLevel)) {
+                this.setState({ gotGameInfo: true });
+            }
+        }
+        event.preventDefault();
+    }
+    setDifficultyLevel(event) {
+        this.setState({ difficultyLevel: event.target.value });
+    }
+    reset() {
+        this.setState({ mode: null, piece: null, turn: null, gotGameInfo: false });
+    }
+    
     render() {
-		if( this.state.gotGameInfo ) {
-			return (
-				<div className="game">
-					<div className="game-board">
-					  <Board mode={this.state.mode} piece={this.state.piece} turn={this.state.turn}/>
-					</div>
-					<div id="resetSection"> <br/><button id = "reset" onClick={() => this.reset()}>Play again</button></div>
-				</div>
-			);
-		} else {
-			return (
-				<form onSubmit={this.handleSubmit}>
-					
-					Choose Game mode:
-					<input type="radio" name="mode" value="one" onChange={this.handleModeChange}/> One Player
-					<input type="radio" name="mode" value="two" onChange={this.handleModeChange}/> Two Player						
-					<br/>
-					
-					Choose 'X' or '0':
-					<input type="radio" name="piece" value="X" onChange={this.handlePieceChange}/> X
-					<input type="radio" name="piece" value="0" onChange={this.handlePieceChange}/> 0				
-					<br/>
-					
-					Which turn ?
-					<input type="radio" name="turn" value="first" onChange={this.handleTurnChange}/> First
-					<input type="radio" name="turn" value="second" onChange={this.handleTurnChange}/> Second			
-					<br/>
-					<input type="submit" value="Submit" />
-				</form>
-			);
-		}
+        let levelInput = (
+            <div>
+                Choose Difficulty level: 
+                <input type="radio" name="level" value="easy" onChange={this.setDifficultyLevel} /> Easy
+                <input type="radio" name="level" value="med" onChange={this.setDifficultyLevel} /> Moderate
+                <input type="radio" name="level" value="diff" onChange={this.setDifficultyLevel} /> Tough
+            </div>
+        );
+
+
+        if (this.state.gotGameInfo) {
+            return (
+                <div className="game">
+                    <div className="game-board">
+                        <Board mode={this.state.mode} difficultyLevel={this.state.difficultyLevel}
+                            piece={this.state.piece} turn={this.state.turn} />
+                    </div>
+                    <div id="resetSection"> <br /><button id="reset" onClick={() => this.reset()}>Play again</button></div>
+                </div>
+            );
+        } else {
+            return (
+                <form onSubmit={this.handleSubmit}>
+
+                    Choose Game mode:
+					<input type="radio" name="mode" value="one" onChange={this.handleModeChange} /> One Player
+					<input type="radio" name="mode" value="two" onChange={this.handleModeChange} /> Two Player
+					<br />
+
+                    {this.state.mode === 'one' ? levelInput : null}
+
+                    Choose 'X' or '0':
+					<input type="radio" name="piece" value="X" onChange={this.handlePieceChange} /> X
+					<input type="radio" name="piece" value="0" onChange={this.handlePieceChange} /> 0
+					<br />
+
+                    Which turn ?
+					<input type="radio" name="turn" value="first" onChange={this.handleTurnChange} /> First
+					<input type="radio" name="turn" value="second" onChange={this.handleTurnChange} /> Second
+					<br />
+                    <input type="submit" value="Submit" />
+                </form>
+            );
+        }
     }
 }
 
